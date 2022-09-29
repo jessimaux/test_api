35d752214008036e31aea865aafb8a789c620ca6
jessimaux <jessimaux@gmail.com>
2022-09-29 12:06:04 +0300

    add swagger

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 from rest_framework import routers
 
@@ -10,11 +9,8 @@ from rest_framework import routers
 urlpatterns = [
     path('api/v1/women/', views.WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', views.WomenAPIUpdate.as_view()),
-    path('api/v1/womendelete/<int:pk>/', views.WomenAPIDestroy.as_view()),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     # path('api/v1/', include(router.urls)),
     # path('api/v1/womenlist', views.WomenViewSet.as_view({'get': 'list'})),
     # path('api/v1/womenlist/<int:pk>/', views.WomenViewSet.as_view({'put': 'update'})),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework import permissions
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(
+        title="Snippets API",
+        version='v1',
+        description="Test description",
+        patterns=[path('api/v1/', include('api.urls')), ],
+        public=True,
+        permission_classes=[permissions.AllowAny,],
+    )
 
 urlpatterns = [
+    path('openapi', schema_view, name='openapi-schema'),
+    path(  # new
+        'swagger-ui/',
+        TemplateView.as_view(
+            template_name='swaggerui/swaggerui.html',
+            extra_context={'schema_url': 'openapi-schema'}
+        ),
+        name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
 ]
